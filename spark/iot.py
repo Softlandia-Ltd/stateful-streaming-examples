5257ba21c366c256ffaeecfb61ced3c84cc6fdb2
mikko <mikko.lehtimaki@softlandia.fi>
2023-02-14 22:32:48 +0200

    Remove unnecessary intermediate variable from spark example

--- spark/iot.py
+++ spark/iot.py
@@ -23,25 +23,23 @@ def main(addr: str = "127.0.0.1:9092", topic: str = "iot", win: int = 10):
         .option("kafka.bootstrap.servers", addr)
         .option("subscribe", topic)
         .load()
-    )
-
-    # Here we make a new DataFrame with just the JSON payload in one field
-    json_df = stream_df.select(
+    ).select(
+        # Here we make a new DataFrame with just the JSON payload in one field
         from_json(col("value").cast("string"), "id INT, time DOUBLE, value INT").alias(
             data_col
         )
     )
 
     # Unpack the JSON, transform event time to correct format
-    value_df = json_df.select(
+    value_df = stream_df.select(
         col(f"{data_col}.id"),
         col(f"{data_col}.value"),
         to_timestamp(from_unixtime(col(f"{data_col}.time"))).alias(ts_col),
     )
 
     # Tumbling window and grouping
+    # Without the watermark we cannot use the append mode, only update and complete
     windowed_sum_df = (
-        # Without the watermark we cannot use the append mode, only update and complete
         value_df.withWatermark(ts_col, f"{win} seconds").groupBy(
             window(ts_col, f"{win} seconds"), value_df.id
         )
